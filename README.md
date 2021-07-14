0ea233c0e80c144243636bfec74fa4edf73f91cc
sera <nrseraphin@gmail.com>
2021-07-14 14:59:21 +0300

    feat : add months variable july to december

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ Include the files used
 ### Credits
 It's important to give proper credit. Add links to any repo that inspired you or blogposts you consulted.
 
+
+
+
+
+
+Add change in this file under the refactoring branch
--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-months = ['january', 'february', 'march', 'april', 'may', 'june']
+months = ['january', 'february', 'march', 'april', 'may', 'june','july','august','october','november','september','december']
 days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
 def get_filters():
     """
